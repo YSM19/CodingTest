e9b737012a73dbcbe2a0aec29df963aee9660784
ysm <clov7153@gmail.com>
2024-02-21 02:00:33 +0900

    2024-02-20

---
+++ BOJ/src/Bronze3/Day2/BOJ2444.java
@@ -0,0 +1,16 @@
+package Bronze3.Day2;
+
+import java.util.Scanner;
+
+public class BOJ2444 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = Integer.parseInt(sc.nextLine());
+
+        for (int i=0; i<n; i++){
+
+        }
+        System.out.println();
+    }
+}
