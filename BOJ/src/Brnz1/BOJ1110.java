40f8ff331d62578569ecde9334f31d794f050a03
YSM <clov7153@gmail.com>
2024-03-05 13:05:18 +0900

    2024-03-05

--- BOJ/src/Brnz1/BOJ1110.java
+++ BOJ/src/Brnz1/BOJ1110.java
@@ -5,12 +5,22 @@ import java.util.Scanner;
 public class BOJ1110 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        String n = sc.next();
 
-        int n = sc.nextInt();
+        String init = n;
+        int cnt = 0;
 
-        if(n<10){
-            
+        while (true) {
+            int sum = n.charAt(0) - '0' + n.charAt(1) - '0';
+
+            n = n.charAt(1) + sum + "";
+            if(init.equals(n)) break;
+            if(Integer.parseInt(n)<10){
+                n = '0'+n;
+            }
+            cnt++;
+
         }
-
+        System.out.println(cnt);
     }
 }
