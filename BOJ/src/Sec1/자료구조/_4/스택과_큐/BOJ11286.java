2620cbe4ef1c1d7aad13da4646bc9d2ac0ccd333
YSM <clov7153@gmail.com>
2024-03-14 16:04:30 +0900

    2024-03-14

--- BOJ/src/Sec1/자료구조/_4/스택과_큐/BOJ11286.java
+++ BOJ/src/Sec1/자료구조/_4/스택과_큐/BOJ11286.java
@@ -3,12 +3,35 @@ package Sec1.자료구조._4.스택과_큐;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.*;
 
 public class BOJ11286 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        
+        PriorityQueue<Integer> q = new PriorityQueue<>((o1, o2)-> {
 
+            int first_abs = Math.abs(o1);
+            int second_abs = Math.abs(o2);
+            if(first_abs == second_abs) { // 절댓값이 같은 경우 음수 우선
+                return o1 > o2 ? 1 : -1;
+            }
+            return first_abs - second_abs; // 절댓값 작은 데이터 우선
+        });
+
+        for (int i=0; i<n; i++) {
+            int num = Integer.parseInt(br.readLine());
+            if (num == 0){
+                if (q.isEmpty()){
+                    System.out.println(0);
+                }
+                for (int j=0; j<q.size(); j++) {
+                    System.out.println(q.poll());
+                }
+            }
+            else {
+                q.add(num);
+            }
+        }
     }
 }
---
+++ BOJ/src/Sec2/Greedy/BOJ1541.java
@@ -0,0 +1,30 @@
+package Sec2.Greedy;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ1541 {
+    static int ans = 0;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+        String[] str = s.split("-");
+        for (int i=0; i<str.length; i++) {
+            int temp = mySum(str[i]);
+            if (i==0) ans = ans+ temp;
+            else ans = ans - temp;
+        }
+        System.out.println(ans);
+
+    }
+
+    private static int mySum(String s) {
+        int sum = 0;
+        String[] temp = s.split("[+]");
+        for (int i=0; i<temp.length; i++) {
+            sum = sum + Integer.parseInt(temp[i]);
+        }
+        return sum;
+    }
+}
