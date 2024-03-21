ba2f764c6cf6bf5601a7008b4ce4beb1d4b65e6a
YSM <clov7153@gmail.com>
2024-03-21 17:12:02 +0900

    2024-03-21

---
+++ BOJ/src/Sec2/Greedy/BOJ14916.java
@@ -0,0 +1,26 @@
+package Sec2.Greedy;
+
+import java.util.Scanner;
+
+public class BOJ14916 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int cnt = 0;
+
+        while (n > 0){
+            if (n%5 == 0){
+                cnt += n/5;
+                break;
+            }
+            else {
+                n -= 2;
+                cnt++;
+            }
+        }
+        if (n < 0) cnt = -1;
+
+        System.out.println(cnt);
+
+    }
+}
