4bbefaabc5738831387d4b98136c273e2fe188f2
ysm <clov7153@gmail.com>
2024-03-22 16:40:25 +0900

    temp

--- BOJ/src/Sec1/자료구조/_4/스택과_큐/BOJ11286.java
+++ BOJ/src/Sec1/자료구조/_4/스택과_큐/BOJ11286.java
@@ -8,7 +8,7 @@ public class BOJ11286 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        
+
 
     }
 }
