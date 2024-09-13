889365baaa5ee2037c104565a20ba7aa2f7c3523
YSM <clov7153@gmail.com>
2024-09-13 17:05:37 +0900

    2024-09-13

--- BOJ/src/solvedac/level3/try_1/BOJ11723.java
+++ BOJ/src/solvedac/level3/try_1/BOJ11723.java
@@ -1,7 +1,26 @@
 package solvedac.level3.try_1;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class BOJ11723 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int M = Integer.parseInt(br.readLine());
+
+        StringTokenizer st;
+        for(int i=0; i<M; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            switch (st.nextToken()) {
+                case "add":
+
+            }
+
+            if(st.nextToken())
+        }
 
     }
 }
